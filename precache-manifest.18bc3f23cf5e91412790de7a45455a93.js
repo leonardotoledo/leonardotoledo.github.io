ec243bb54b49428bfaf73b5b7888ef65d471131b
Leonardo Tolêdo <leonardotoledo@outlook.com>
2022-06-10 21:08:46 -0300

    Updates

---
+++ precache-manifest.18bc3f23cf5e91412790de7a45455a93.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "144b994062b9fff7870f",
+    "url": "/static/js/main.144b9940.chunk.js"
+  },
+  {
+    "revision": "06ed7020fbf189b562b7",
+    "url": "/static/js/1.06ed7020.chunk.js"
+  },
+  {
+    "revision": "8f3220cc03db325a9b0e",
+    "url": "/static/js/2.8f3220cc.chunk.js"
+  },
+  {
+    "revision": "21a7007f90bff1f3f2c7",
+    "url": "/static/js/runtime~main.21a7007f.js"
+  },
+  {
+    "revision": "49d9de5419d094450f3158e32f6f4d87",
+    "url": "/static/media/terrain.49d9de54.jpg"
+  },
+  {
+    "revision": "9d81b43cbf149cdc40880588fd6ba5a0",
+    "url": "/static/media/about.9d81b43c.md"
+  },
+  {
+    "revision": "9e472196a9d590bcb8e92b09d3934e7d",
+    "url": "/static/media/scientific.9e472196.md"
+  },
+  {
+    "revision": "11f3208f1bd18d3d1d7ac0580bff2e31",
+    "url": "/static/media/translation.11f3208f.md"
+  },
+  {
+    "revision": "b9f0ef745352a86559b83bd90c1b0c6b",
+    "url": "/static/media/web.b9f0ef74.md"
+  },
+  {
+    "revision": "87a53831cd037385780000bc175b9c8f",
+    "url": "/index.html"
+  }
+];
